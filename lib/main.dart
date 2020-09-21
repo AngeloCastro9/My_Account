135dc872b8aecc339ce6504e0752f37a1a7d54f8
angelocastro9 <castromatheus060@gmail.com>
2020-09-21 14:31:12 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Consumption',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'My-Consumption'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  double _totalAccount= 0;
+  double _valuePrecision = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      _totalAccount = (_counter * 4.9);
+      _valuePrecision = num.parse(_totalAccount.toStringAsPrecision(4));
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      _totalAccount = _counter * 4.9;
+      _valuePrecision = num.parse(_totalAccount.toStringAsPrecision(4));
+      if(_counter <= 0){
+        _counter = 0;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Your chopps:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              onPressed: _decrementCounter,
+              tooltip: 'Decrement',
+              child: Icon(Icons.remove),
+            ),
+            Text(
+              '$_valuePrecision'
+            )
+
+          ],
+        ),
+      ),
+        // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
