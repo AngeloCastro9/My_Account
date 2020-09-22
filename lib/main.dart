9ea75acacfd096d328928b8b0514e932d41215e9
angelocastro9 <castromatheus060@gmail.com>
2020-09-22 09:21:27 -0300

    code refactoring and water add

--- lib/main.dart
+++ lib/main.dart
@@ -27,69 +27,138 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  double _totalAccount= 0;
-  double _valuePrecision = 0;
+  int _counterChopp = 0;
+  double _totalAccountChopp= 0;
+  double _valuePrecisionChopp = 0;
+  int _counterWater = 0;
+  double _totalAccountWater= 0;
+  double _valuePrecisionWater = 0;
 
-  void _incrementCounter() {
+  // begin chopp
+  void _incrementCounterChopp() {
     setState(() {
-      _counter++;
-      _totalAccount = (_counter * 4.9);
-      _valuePrecision = num.parse(_totalAccount.toStringAsPrecision(4));
+      _counterChopp++;
+      _totalAccountChopp = (_counterChopp * 4.9);
+      _valuePrecisionChopp = num.parse(_totalAccountChopp.toStringAsPrecision(4));
     });
   }
 
-  void _decrementCounter() {
+  void _decrementCounterChopp() {
     setState(() {
-      _counter--;
-      _totalAccount = _counter * 4.9;
-      _valuePrecision = num.parse(_totalAccount.toStringAsPrecision(4));
-      if(_counter <= 0){
-        _counter = 0;
+      _counterChopp--;
+      _totalAccountChopp = _counterChopp * 4.9;
+      _valuePrecisionChopp = num.parse(_totalAccountChopp.toStringAsPrecision(4));
+      if(_counterChopp <= 0){
+        _counterChopp = 0;
       }
-      if(_valuePrecision <= 0){
-        _valuePrecision = 0;
+      if(_valuePrecisionChopp <= 0){
+        _valuePrecisionChopp = 0;
       }
     });
   }
+  // final chopp
 
+  // begin water
+  void _incrementCounterWater() {
+    setState(() {
+      _counterWater++;
+      _totalAccountWater = (_counterWater * 6.9);
+      _valuePrecisionWater = num.parse(_totalAccountWater.toStringAsPrecision(4));
+    });
+  }
+
+  void _decrementCounterWater() {
+    setState(() {
+      _counterWater--;
+      _totalAccountWater = _counterWater * 6.9;
+      _valuePrecisionWater = num.parse(_totalAccountWater.toStringAsPrecision(4));
+      if(_counterWater <= 0){
+        _counterWater = 0;
+      }
+      if(_valuePrecisionWater <= 0){
+        _valuePrecisionWater = 0;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        height: 130.0,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FloatingActionButton(
-              onPressed: _decrementCounter,
-              tooltip: 'Decrement',
-              child: Icon(Icons.remove),
-            ),
-            Image.asset(
-              'assets/images/chopp.png',
-              fit: BoxFit.cover,
-            ),
-            FloatingActionButton(
-              onPressed: _incrementCounter,
-              tooltip: 'Increment',
-              child: Icon(Icons.add),
-            ),
-            Spacer(),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Spacer(),
-            Text(
-              'RS $_valuePrecision',
-                style: Theme.of(context).textTheme.display1,
-            )
-          ],
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: new Container(
+         // color: Color(0xff258DED),
+          height: 400.0,
+          alignment: Alignment.center,
+          child: new Column(
+            children: [
+              new Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FloatingActionButton(
+                        onPressed: _decrementCounterChopp,
+                        tooltip: 'Decrement',
+                        child: Icon(Icons.remove),
+                      ),
+                      Image.asset(
+                        'assets/images/chopp.png',
+                        fit: BoxFit.cover,
+                      ),
+                      FloatingActionButton(
+                        onPressed: _incrementCounterChopp,
+                        tooltip: 'Increment',
+                        child: Icon(Icons.add),
+                      ),
+                      Spacer(),
+                      Text(
+                        '$_counterChopp',
+                        style: Theme.of(context).textTheme.display1,
+                      ),
+                      Spacer(),
+                      Text(
+                        'RS $_valuePrecisionChopp',
+                        style: Theme.of(context).textTheme.display1,
+                      ),
+                    ],
+                ),
+              ),
+
+              new Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: _decrementCounterWater,
+                      tooltip: 'Decrement',
+                      child: Icon(Icons.remove),
+                    ),
+                    Image.asset(
+                      'assets/images/water.png',
+                      fit: BoxFit.cover,
+                    ),
+                    FloatingActionButton(
+                      onPressed: _incrementCounterWater,
+                      tooltip: 'Increment',
+                      child: Icon(Icons.add),
+                    ),
+                    Spacer(),
+                    Text(
+                      '$_counterWater',
+                      style: Theme.of(context).textTheme.display1,
+                    ),
+                    Spacer(),
+                    Text(
+                      'RS $_valuePrecisionWater',
+                      style: Theme.of(context).textTheme.display1,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
